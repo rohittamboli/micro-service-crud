c5e6131d1c20ee5804bb25c29b41cf11a7ddbc21
rohit tamboli <rohittamboli.rt34@gmail.com>
2020-06-26 12:18:59 +0530

    Add files via upload

---
+++ SpringBoot_AccelerateTask /src/main/java/com/SpringBoot_AccelerateTask/SpringBootAccelerateTaskApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_AccelerateTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAccelerateTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAccelerateTaskApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_AccelerateTask /src/test/java/com/SpringBoot_AccelerateTask/SpringBootAccelerateTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_AccelerateTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAccelerateTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
