14665bab8ab0b68fa70d44cf8aad8cbe4c0b7fad
rohit tamboli <rohittamboli.rt34@gmail.com>
2020-06-26 10:28:49 +0530

    Add files via upload

---
+++ SpringBoot_AccelerateTask/src/test/java/com/infy/bank/SpringBoot_AccelerateTask/SpringBootAccelerateTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.bank.SpringBoot_AccelerateTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAccelerateTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
